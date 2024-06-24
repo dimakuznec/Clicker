2728f3e1eec43779a62d7f9b2029bf56e6c64251
Dima Kuznecov <kud3544@gmail.com>
2024-06-24 15:05:09 +0300

    git commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -9,13 +9,14 @@ import Shop from './components/Shop/Shop'
 import Upgrade from './components/Upgrade/Upgrade'
 
 const App: React.FC = () => {
+	// Состояния приложения
 	const [currency, setCurrency] = useState<number>(() => {
 		const saved = localStorage.getItem('currency')
 		return saved ? Number(saved) : 0
 	})
 	const [currentSkin, setCurrentSkin] = useState<string>(() => {
 		const saved = localStorage.getItem('currentSkin')
-		return saved || 'gray'
+		return saved || 'red' // Начальный цвет на красный
 	})
 	const [clickIncrement, setClickIncrement] = useState<number>(() => {
 		const saved = localStorage.getItem('clickIncrement')
@@ -40,6 +41,52 @@ const App: React.FC = () => {
 		const saved = localStorage.getItem('ownedSkins')
 		return saved ? JSON.parse(saved) : []
 	})
+	const [isActive, setIsActive] = useState<boolean>(true)
+
+	// Рефы для таймеров и интервалов
+	const passiveIncomeInterval = useRef<number | null>(null)
+	const inactivityTimeout = useRef<number | null>(null)
+
+	// Обработка активности пользователя
+	const handleUserActivity = () => {
+		setIsActive(true)
+		if (inactivityTimeout.current) {
+			clearTimeout(inactivityTimeout.current)
+		}
+		inactivityTimeout.current = window.setTimeout(() => {
+			setIsActive(false)
+		}, 3 * 60 * 1000) // 3 минуты
+	}
+
+	// Эффект для обработки активности пользователя
+	useEffect(() => {
+		const handleActivity = () => handleUserActivity()
+		window.addEventListener('mousemove', handleActivity)
+		window.addEventListener('keydown', handleActivity)
+		window.addEventListener('click', handleActivity)
+
+		return () => {
+			window.removeEventListener('mousemove', handleActivity)
+			window.removeEventListener('keydown', handleActivity)
+			window.removeEventListener('click', handleActivity)
+		}
+	}, [])
+
+	// Эффект для пассивного дохода
+	useEffect(() => {
+		if (isActive && autoFarmLevel > 1) {
+			if (!passiveIncomeInterval.current) {
+				passiveIncomeInterval.current = window.setInterval(() => {
+					setCurrency(prevCurrency => prevCurrency + autoFarmLevel)
+				}, 10000) // Каждые 10 секунд
+			}
+		} else {
+			if (passiveIncomeInterval.current) {
+				clearInterval(passiveIncomeInterval.current)
+				passiveIncomeInterval.current = null
+			}
+		}
+	}, [isActive, autoFarmLevel])
 
 	// Сохранение состояния в localStorage при изменении
 	useEffect(() => {
@@ -70,11 +117,13 @@ const App: React.FC = () => {
 		localStorage.setItem('coinFlipTimer', coinFlipTimer.toString())
 	}, [coinFlipTimer])
 
+	// Обработка клика
 	const handleClick = () => {
 		setCurrency(prevCurrency => prevCurrency + clickIncrement)
-		setClickAnimation(true) // Запуск анимации
+		setClickAnimation(true) // Запуск анимации клика
 	}
 
+	// Покупка скина
 	const handleBuySkin = (skin: string, cost: number) => {
 		if (currency >= cost && !ownedSkins.includes(skin)) {
 			setCurrency(prevCurrency => prevCurrency - cost)
@@ -83,6 +132,7 @@ const App: React.FC = () => {
 		}
 	}
 
+	// Прокачка уровня
 	const handleUpgrade = (cost: number, increment: number) => {
 		if (currency >= cost) {
 			setCurrency(prevCurrency => prevCurrency - cost)
@@ -91,7 +141,8 @@ const App: React.FC = () => {
 		}
 	}
 
-	const handleAutoFarmUpgrade = (cost: number, increment: number) => {
+	// Прокачка уровня автофермы
+	const handleAutoFarmUpgrade = (cost: number) => {
 		if (currency >= cost) {
 			setCurrency(prevCurrency => prevCurrency - cost)
 			setAutoFarmLevel(prevLevel => prevLevel + 1)
@@ -115,53 +166,58 @@ const App: React.FC = () => {
 		}
 	}, [coinFlipTimer])
 
-	// Эффект для автофарминга
-	useEffect(() => {
-		if (autoFarmLevel > 1) {
-			const autoFarmInterval = setInterval(() => {
-				setCurrency(prevCurrency => prevCurrency + autoFarmLevel)
-			}, 1000)
-
-			return () => clearInterval(autoFarmInterval)
-		}
-	}, [autoFarmLevel])
-
 	// Эффект для анимации клика
 	useEffect(() => {
 		if (clickAnimation) {
 			const animationTimeout = setTimeout(() => {
-				setClickAnimation(false) // Остановка анимации
+				setClickAnimation(false) // Остановка анимации клика
 			}, 1000) // Длительность анимации должна соответствовать CSS анимации
 
 			return () => clearTimeout(animationTimeout)
 		}
 	}, [clickAnimation])
 
+	// Игра "Орёл и решка"
 	const handleCoinFlip = (choice: string) => {
 		const result = Math.random() < 0.5 ? 'Орёл' : 'Решка'
 		if (choice === result) {
 			setCurrency(prevCurrency => prevCurrency + 50)
 			setUpgradeLevel(prevLevel => prevLevel + 1)
 			setAutoFarmLevel(prevLevel => prevLevel + 1)
-			setClickIncrement(prevIncrement => prevIncrement + 1) // Увеличение increment при выигрыше
+			setClickIncrement(prevIncrement => prevIncrement + 1) // Увеличение инкремента при выигрыше
 			toast.success('Вы выиграли! Уровень повышен и вы получили 50 монет.')
 		} else {
 			setUpgradeLevel(prevLevel => Math.max(1, prevLevel - 1))
 			setAutoFarmLevel(prevLevel => Math.max(1, prevLevel - 1))
-			setClickIncrement(prevIncrement => Math.max(1, prevIncrement - 1)) // Уменьшение increment при проигрыше
+			setClickIncrement(prevIncrement => Math.max(1, prevIncrement - 1)) // Уменьшение инкремента при проигрыше
 			toast.error('Вы проиграли! Уровень понижен.')
 		}
 		setShowCoinFlipModal(false) // Закрыть модальное окно игры "Орёл и решка" после окончания игры
 	}
 
+	// Закрытие модального окна
 	const handleCloseModal = () => {
 		setShowCoinFlipModal(false) // Закрыть модальное окно вручную по требованию
+	}
+
+	// Сброс всех данных
+	const resetAll = () => {
+		setCurrency(0)
+		setCurrentSkin('red') // Установить начальный цвет на красный
+		setClickIncrement(1)
+		setUpgradeLevel(1)
+		setAutoFarmLevel(1) // Начальный уровень автофермы устанавливается на 1
+		setOwnedSkins([])
+		setCoinFlipTimer(300)
+		toast.info('Все данные сброшены.')
+		localStorage.clear()
 	}
 
 	return (
 		<Router>
 			<div className='App'>
 				<ToastContainer />
+				{/* Навигационная панель */}
 				<nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
 					<div className='burger' onClick={() => setIsMenuOpen(!isMenuOpen)}>
 						<span></span>
@@ -174,6 +230,8 @@ const App: React.FC = () => {
 						<Link to='/upgrade'>Прокачка</Link>
 					</div>
 				</nav>
+
+				{/* Маршруты */}
 				<Routes>
 					<Route
 						path='/'
@@ -215,6 +273,8 @@ const App: React.FC = () => {
 						}
 					/>
 				</Routes>
+
+				{/* Модальное окно для игры "Орёл и решка" */}
 				{showCoinFlipModal && (
 					<CoinFlipModal
 						show={showCoinFlipModal}
@@ -222,6 +282,11 @@ const App: React.FC = () => {
 						onPlay={handleCoinFlip}
 					/>
 				)}
+
+				{/* Кнопка сброса данных */}
+				<button onClick={resetAll} className='reset-button'>
+					Сбросить всё
+				</button>
 			</div>
 		</Router>
 	)
